f2c67c9c65b10d4bedbaadf6d9bc7dedd039773b
Alberto <80765270+VicFleur@users.noreply.github.com>
2022-12-19 23:32:41 +0100

    Avoid import

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -3,9 +3,14 @@ import lazyimagesOptions from '#lazyimages-options'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
 
-    const lazySizes = await import('lazysizes')
+    const scripts = []
+    scripts.push({
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js',
+        async: true
+    })
+    if (Object.keys(lazyimagesOptions).length > 0) scripts.push({ children: `window.lazySizesConfig = ${JSON.stringify(lazyimagesOptions)}` })
 
-    // Assign custom options to Lazysizes
-    Object.assign(lazySizes.cfg, lazyimagesOptions)
-    
+    useHead({
+        script: scripts
+    })
 })
